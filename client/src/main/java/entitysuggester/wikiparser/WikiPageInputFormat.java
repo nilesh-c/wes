c3be1aaf8567b488453faacf727ed8dc135b15a9
Nilesh Chakraborty <nilesh@nileshc.com>
2013-07-03 14:37:58 +0530

    Added InputFormat files for hadoop to parse Wiki dumps
    Change-Id: I6591a54266b8ed2078e5362ce7d0e05216a239a6

---
+++ client/src/main/java/entitysuggester/wikiparser/WikiPageInputFormat.java
@@ -0,0 +1,35 @@
+package entitysuggester.wikiparser;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+
+import graphfu.XMLInputFormat;
+import graphfu.XMLInputFormat.XMLRecordReader;
+
+public class WikiPageInputFormat extends FileInputFormat<LongWritable, Text> {
+
+  public static final String START_TAG = "<page>";
+  public static final String END_TAG = "</page>";
+
+  @Override
+  public RecordReader<LongWritable, Text> getRecordReader(InputSplit split,
+      JobConf conf, Reporter reporter) throws IOException {
+    conf.set(XMLInputFormat.START_TAG_KEY, START_TAG);
+    conf.set(XMLInputFormat.END_TAG_KEY, END_TAG);
+    return new XMLRecordReader((FileSplit) split, conf);
+  }
+}
