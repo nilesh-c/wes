fb0e13e681ff9bbb860a03b68e3f27239631a06d
Nilesh Chakraborty <nilesh@nileshc.com>
2013-07-09 01:57:35 +0530

    Fixed errors in parser scripts
    Change-Id: Icb14b41875e3e86ba63495a46d31af2c27d56c51

--- wikiparser/wikiparser_r.py
+++ wikiparser/wikiparser_r.py
@@ -3,9 +3,14 @@
 import sys
 
 def main():
+    listout = open(sys.argv[1], "w")
     for i in sys.stdin:
         (key, value) = i.split("\t")
-        sys.stdout.write(value + "\n")
+        if key == "@@":
+            sys.stdout.write(value)
+        else:
+            listout.write(value)
+    listout.close()
         
 if __name__ == '__main__':
     main()
