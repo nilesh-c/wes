710cee2f3fb86f5d4a5eb485f79484572218af45
Nilesh <nilesh@nileshc.com>
2013-07-23 23:57:18 +0530

    Removed some autogenerated comments
    Change-Id: Ie8937e52a1fb72416626bcacd1fb672ba0f73ffd

--- client/src/main/java/org/wikimedia/wikibase/entitysuggester/client/recommender/AbstractClientRecommender.java
+++ client/src/main/java/org/wikimedia/wikibase/entitysuggester/client/recommender/AbstractClientRecommender.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.wikimedia.wikibase.entitysuggester.client.recommender;
 
 import java.io.IOException;
